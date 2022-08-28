c3809d0d011a9643cd7bf00294d1ec9a7498377e
mohamed-abdulla <abdabdulla78@gmail.com>
2022-08-28 22:25:57 +0530

    update

--- pages/api/search/[searchTerm].ts
+++ pages/api/search/[searchTerm].ts
@@ -4,7 +4,7 @@ import { searchPostsQuery } from "../../../utils/queries";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "GET") {
-    const { searchTerm } = req.query;
+    const { searchTerm }: any = req.query;
     const videosQuery = searchPostsQuery(searchTerm);
 
     const videos = await client.fetch(videosQuery);
